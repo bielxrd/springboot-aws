8104b747aed063335e3547a9221cc961d191e206
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-06-04 22:06:27 -0300

    initial commit

---
+++ Studying-AWS-Services/src/main/java/br/com/aws/study/StudyingAwsServicesApplication.java
@@ -0,0 +1,13 @@
+package br.com.aws.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyingAwsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyingAwsServicesApplication.class, args);
+	}
+
+}
---
+++ Studying-AWS-Services/src/test/java/br/com/aws/study/StudyingAwsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aws.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyingAwsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
