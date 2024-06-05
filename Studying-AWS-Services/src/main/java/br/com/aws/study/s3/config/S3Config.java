9b7a8d1cde56e36624a8d4ee9ad15dfbeea6004a
Gabriel Dias <gabriel.diasramos@hotmail.com>
2024-06-04 23:48:41 -0300

    classe configuracao para credencials e region da aws

---
+++ Studying-AWS-Services/src/main/java/br/com/aws/study/s3/config/S3Config.java
@@ -0,0 +1,33 @@
+package br.com.aws.study.s3.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Value("${aws.accessKeyId}")
+    private String accessKey;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
+    @Bean
+    public AmazonS3 amazonS3() {
+        Regions region = Regions.SA_EAST_1;
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+        return AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(region)
+                .build();
+    }
+
+}
